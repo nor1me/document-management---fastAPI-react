03df81f90039370adc6e1dcf36eb27587036a2c7
noir <97950651+nor1me@users.noreply.github.com>
2026-05-11 01:04:15 +0800

    .

--- .copier/update_dotenv.py
+++ .copier/update_dotenv.py
@@ -1,9 +1,7 @@
 from pathlib import Path
 import json
 
-# Update the .env file with the answers from the .copier-answers.yml file
-# without using Jinja2 templates in the .env file, this way the code works as is
-# without needing Copier, but if Copier is used, the .env file will be updated
+
 root_path = Path(__file__).parent.parent
 answers_path = Path(__file__).parent / ".copier-answers.yml"
 answers = json.loads(answers_path.read_text())
